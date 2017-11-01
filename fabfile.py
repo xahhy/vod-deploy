d07953afb2ab957b02e274dbaf110679ff5532ea
hbhehongyuan <1014596312@qq.com>
2017-11-01 18:00:33 +0800

    添加命令行参数接口

--- fabfile.py
+++ fabfile.py
@@ -1,3 +1,4 @@
+# coding:utf8
 from fabric import *
 from fabric.context_managers import cd
 from fabric.decorators import task
@@ -12,29 +13,40 @@ SOFTWARES = [
     'gnome-mplayer',
     'python-pip',
     'python3-pip',
+    'mysql'
 ]
 PACKAGE_DIR = '/home/tongshi/Desktop/packages'
 
 
 @task
+def install(text):
+    """通过命令行使用apt-get安装软件,fab install:xxxx"""
+    sudo('apt-get install -y %s' % text)
+
+
+@task
 def update_apt():
+    """安装完Ubuntu系统后更新apt仓库"""
     sudo('apt update -y')
     sudo('apt upgrade -y')
 
 
 @task
 def install_software():
+    """使用apt-get安装SOFTWARE列表中的软件"""
     software = ' '.join(SOFTWARES)
     sudo('apt-get install -y %s' % software)
 
 
 @task
 def copy_files():
+    """拷贝安装包"""
     put(PACKAGE_DIR, '/home/tongshi/Desktop/')
 
 
 @task
 def install_ethernet():
+    """安装万兆网卡驱动"""
     with settings(warn_only=True):
         with cd(PACKAGE_DIR):
             if run('test -d mlnx*').failed:
